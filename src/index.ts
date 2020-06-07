706b2615d383b9f4f6018e40e313fca34a72c006
Harman Bhatti <harman@Harmans-MacBook-Pro.local>
2020-06-08 02:14:50 +0530

    current user test route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express'
-import { json } from 'body-parser'
-
+import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentUser';
 const app = express();
 app.use(json());
 
+app.use('/api/users/currentuser', currentUserRouter);
+
 app.listen(3000, () => {
-  console.log('Listenting on PORT 3000!')
+  console.log('Listenting on PORT 3000!!!!')
 })
