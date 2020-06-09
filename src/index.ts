0b2825fe4249bb81e3545e807c7b5fe6074d64bb
Harman Bhatti <harman@Harmans-MacBook-Pro.local>
2020-06-09 21:39:46 +0530

    validation on signup route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { json } from 'body-parser';
 import { currentUserRouter } from './routes/currentUser';
 import { signUpRouter } from './routes/signup';
 import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
 
 const app = express();
 app.use(json());
@@ -10,6 +11,7 @@ app.use(json());
 app.use('/api/users/currentuser', currentUserRouter);
 app.use('/api/users/signup', signUpRouter);
 app.use('/api/users/signin', signInRouter);
+app.use('/api/users/signout', signOutRouter);
 
 app.listen(3000, () => {
   console.log('Listenting on PORT 3000!!!!')
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Welcome to sign up page')
+router.post('/', [
+  check('email')
+    .trim()
+    .isEmail()
+    .withMessage("Email must be valid"),
+  check('password')
+    .trim()
+    .isString()
+    .isLength({min: 5, max: 20})
+    .withMessage("password must be between 5 to 20 characters")
+], (req: Request, res: Response) => {
+  
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  res.send({"email": req.body.email, "password": req.body.password})
+
 });
 
 export { router as signUpRouter}
