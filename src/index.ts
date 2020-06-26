5b5583366ae23bb4f5ab1a502abc1be2c756d7ca
Harman Bhatti <harman@Harmans-MacBook-Pro.local>
2020-06-26 12:22:07 +0530

    connecting to mongo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/currentUser';
 import { signUpRouter } from './routes/signup';
 import { signInRouter } from './routes/signin';
@@ -15,6 +17,21 @@ app.use('/api/users/signin', signInRouter);
 app.use('/api/users/signout', signOutRouter);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listenting on PORT 3000!!!!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('connected to mongo...')
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listenting on PORT 3000!!!!')
+  })
+}
+
+start();
+
